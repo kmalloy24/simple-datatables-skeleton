3ff0ebf5c14a971ec31183b2e54fd20679da2886
Kyle Malloy <kmalloy24@gmail.com>
2024-01-03 10:53:06 -0500

    ssr example

--- src/routes/ssr/+page.svelte
+++ src/routes/ssr/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { DataHandler, type State, Datatable, Th, ThFilter } from '@vincjo/datatables/remote';
+	import { DataHandler, type State, Datatable, Th } from '@vincjo/datatables/remote';
 	import { reload } from './api';
+	import ThFilter from '$lib/components/ssr/ThFilter.svelte';
 
-	const handler = new DataHandler([], { rowsPerPage: 10 });
+	const handler = new DataHandler([], { rowsPerPage: 10, totalRows: 500 });
 	const rows = handler.getRows();
 
 	handler.onChange((state: State) => reload(state));
@@ -14,29 +15,25 @@
 	<table>
 		<thead>
 			<tr>
-				<th>ID</th>
-				<th>Avatar</th>
-				<th>First name</th>
-				<th>Last name</th>
-				<th>Age</th>
-				<th>Gender</th>
-				<th>Height / Weight</th>
+				<Th {handler} orderBy="id">ID</Th>
+				<Th {handler} orderBy="name">Name</Th>
+				<Th {handler} orderBy="email">Email</Th>
+				<Th {handler} orderBy="body">Comment</Th>
+			</tr>
+			<tr>
+				<ThFilter {handler} filterBy="id" />
+				<ThFilter {handler} filterBy="name" />
+				<ThFilter {handler} filterBy="email" />
+				<ThFilter {handler} filterBy="body" />
 			</tr>
 		</thead>
 		<tbody>
 			{#each $rows as row}
 				<tr>
 					<td>{row.id}</td>
-					<td>
-						<img src={row.image} alt="avatar" class="w-8" />
-					</td>
-					<td>{row.firstName}</td>
-					<td>{row.lastName}</td>
-					<td>{row.age}</td>
-					<td>{row.gender}</td>
-					<td>
-						{row.height / 100}m / {row.weight}kg
-					</td>
+					<td><b>{row.name}</b></td>
+					<td>{row.email}</td>
+					<td><p>{row.body}</p></td>
 				</tr>
 			{/each}
 		</tbody>
--- src/routes/ssr/api.ts
+++ src/routes/ssr/api.ts
@@ -1,17 +1,26 @@
 import type { State } from '@vincjo/datatables/remote';
 
 export const reload = async (state: State) => {
-	const response = await fetch(`https://dummyjson.com/users${getParams(state)}`);
-	const json = await response.json();
-	state.setTotalRows(json.total);
-	return json.users;
+	const response = await fetch(`https://jsonplaceholder.typicode.com/comments?${getParams(state)}`);
+	return response.json();
 };
 
-const getParams = ({ offset, rowsPerPage, search }: State) => {
-	const page = `limit=${rowsPerPage}&skip=${offset}`;
+const getParams = (state: State) => {
+	const { pageNumber, rowsPerPage, sort, filters, search } = state;
 
-	if (search) {
-		return `/search?q=${search}&${page}`;
+	let params = `_page=${pageNumber}`;
+
+	if (rowsPerPage) {
+		params += `&_limit=${rowsPerPage}`;
 	}
-	return `?${page}`;
+	if (sort) {
+		params += `&_sort=${sort.orderBy}&_order=${sort.direction}`;
+	}
+	if (filters) {
+		params += filters.map(({ filterBy, value }) => `&${filterBy}=${value}`).join();
+	}
+	if (search) {
+		params += `&q=${search}`;
+	}
+	return params;
 };
